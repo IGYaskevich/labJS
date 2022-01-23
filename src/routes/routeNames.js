b28e340b86cfed645f07c4a03534d9a1e747015d
IgnatYaskevich <78702174+IgnatYaskevich@users.noreply.github.com>
2022-01-23 23:25:04 +0300

    fixed routeNames.js

--- src/routes/routeNames.js
+++ src/routes/routeNames.js
@@ -1,8 +1,8 @@
 export const ROUTE_NAMES = {
-  COUNTER_MANAGER_REDUX: "/labTS",
-  COUNTER_MANAGER: "/labTS/counter_manager",
-  COUNTER: "/labTS/Counter",
-  LAYOUT: "/labTS/layout",
-  TODO: "/labTS/todo",
-  POKEMON: "/labTS/pokemon",
+  COUNTER_MANAGER_REDUX: "/labJS",
+  COUNTER_MANAGER: "/labJS/counter_manager",
+  COUNTER: "/labJS/Counter",
+  LAYOUT: "/labJS/layout",
+  TODO: "/labJS/todo",
+  POKEMON: "/labJS/pokemon",
 };
